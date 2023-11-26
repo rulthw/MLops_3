8745e4bcfd69fbdbc6c8c157cb465144a2186a92
rulthw <rulthw.247@gmail.com>
2023-11-26 14:56:42 +0000

    get_data.py update

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 datafile = 'https://raw.githubusercontent.com/amankharwal/Website-data/master/GlobalTemperatures.csv'
 
-df= pd.read_csv(datafile)
+df = pd.read_csv(datafile)
 
-df.to_csv("~/MLops_3/datasets/data.csv", columns=df.columns)
+df.to_csv("~/MLops_3/datasets/data.csv", index = False)
