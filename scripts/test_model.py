9c50205ca0242981aabb017563c0edcb92adb6a9
rulthw <rulthw.247@gmail.com>
2023-11-30 15:37:03 +0500

    train_model.py, test_model.py update

--- scripts/test_model.py
+++ scripts/test_model.py
@@ -1,6 +1,11 @@
 import pickle
 import pandas as pd
-import os
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+import mlflow
+from mlflow.tracking import MlflowClient
+
+mlflow.set_tracking_uri("http://localhost:5000")
+mlflow.set_experiment("test_model")
 
 df = pd.read_csv('~/MLops_3/datasets/data_test.csv')
 
@@ -10,5 +15,17 @@ y = df['LandAverageTemperature']
 with open('/home/flow/MLops_3/models/model.pkl', 'rb') as f:
     model = pickle.load(f)
 
-score = model.score(X, y)
-print("score=", score)
+with mlflow.start_run():
+    predict = model.predict(X)
+    rmse = mean_squared_error(y, predict, squared=False)
+    mae = mean_absolute_error(y, predict)
+    r2 = r2_score(y, predict)
+    
+    mlflow.log_metric("rmse", rmse)
+    mlflow.log_metric("mae", mae)
+    mlflow.log_metric("r2", r2)
+
+    print("RMSE =", rmse)
+    print("MAE =", mae)
+    print("R2 =", r2)
+    mlflow.end_run()
--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,26 +1,47 @@
 import xgboost as xgb
 import pickle
-import os
 import pandas as pd
+import mlflow
+from mlflow.tracking import MlflowClient
 
-df = pd.read_csv("~/MLops_3/datasets/data_train.csv")
+mlflow.set_tracking_uri("http://localhost:5000")
+mlflow.set_experiment("train_model")
 
-X = df.drop(columns = ["LandAverageTemperature"])
-y = df["LandAverageTemperature"]
+df = pd.read_csv('~/MLops_3/datasets/data_train.csv')
 
-params = {
+X = df.drop(columns = ['LandAverageTemperature'])
+y = df['LandAverageTemperature']
+
+with mlflow.start_run():
+
+    params = {
     "n_estimators":100,
-    "max_depth": 4,
+    "max_depth": 5,
     "learning_rate": 0.01,
     "subsample": 0.8,
-    "colsample_bytree": 0.9,
-    "colsample_bylevel": 0.8,
-    "reg_lambda": 0.1,
-    "random_state": 42,
-}
+    "colsample_bytree": 0.2,
+    "colsample_bylevel": 0.1,
+    "reg_lambda": 0.7,
+    "random_state": 50}
 
-model = xgb.XGBRegressor(**params)
-model.fit(X, y)
+    reg = xgb.XGBRegressor(**params)
 
-with open("/home/flow/MLops_3/models/model.pkl", "wb") as f:
-    pickle.dump(model, f)
+    mlflow.sklearn.log_model(reg,
+                             artifact_path="lr",
+                             registered_model_name="lr")
+    mlflow.log_artifact(local_path="/home/flow/MLops_3/scripts/train_model.py",
+                        artifact_path="train_model code")
+    mlflow.log_param("n_estimators", params["n_estimators"])
+    mlflow.log_param("max_depth", params["max_depth"])
+    mlflow.log_param("learning_rate", params["learning_rate"])
+    mlflow.log_param("subsample", params["subsample"])
+    mlflow.log_param("colsample_bytree", params["colsample_bytree"])
+    mlflow.log_param("colsample_bylevel", params["colsample_bylevel"])
+    mlflow.log_param("reg_lambda", params["reg_lambda"])
+    mlflow.log_param("random_state", params["random_state"])
+    mlflow.end_run()
+
+reg.fit(X, y)
+
+with open('/home/flow/MLops_3/models/model.pkl', 'wb') as f:
+    pickle.dump(reg, f)
